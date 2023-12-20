c909d7195202c6033cb23f5616ae3cfeabe3491d
sagar sakaria <sagar9sakaria@gmail.com>
2023-12-20 23:14:09 +0530

    [ADD] __manifest__.py
    Added manifest

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'My Module',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        'views/my_model_form.xml',
+        'data/my_model_data.xml',  # Include the data file here
+    ],
+    'installable': True,
+}
